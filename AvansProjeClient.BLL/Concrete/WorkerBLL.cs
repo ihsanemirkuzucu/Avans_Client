1761520f10b4da7814c234126cf5fdfcba738ce2
ihsanemirkuzucu <ihsanemirkuzucu44@gmail.com>
2023-12-16 03:45:13 +0300

    düzenleme.

--- AvansProjeClient.BLL/Concrete/WorkerBLL.cs
+++ AvansProjeClient.BLL/Concrete/WorkerBLL.cs
@@ -36,7 +36,6 @@ namespace AvansProjeClient.BLL.Concrete
                 if (result == null)
                 {
                     throw new Exception();
-
                 }
                 return new GeneralReturnType<LoginVM>(result, true, "Giriş Başarılı");
             }
